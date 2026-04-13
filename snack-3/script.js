72f5c070d5e63a3265e4f16f83eeb5bc54fc37f0
Alessandro Ledda <aleledda75@icloud.com>
2026-04-13 19:14:36 +0200

    first commit

---
+++ snack-3/script.js
@@ -0,0 +1,85 @@
+const books = [
+    {
+        title: "React Billionaire",
+        pages: 250,
+        author: {
+            name: 'Alice',
+            age: 35
+        },
+        available: false,
+        price: '101€',
+        tags: ['advanced', 'js', 'react', 'senior']
+    },
+    {
+        title: "Advanced JS",
+        pages: 500,
+        author: {
+            name: 'Bob',
+            age: 20
+        },
+        available: true,
+        price: '25€',
+        tags: ['advanced', 'js', 'mid-senior']
+    },
+    {
+        title: "CSS Secrets",
+        pages: 320,
+        author: {
+            name: 'Alice',
+            age: 17
+        },
+        available: true,
+        price: '8€',
+        tags: ['html', 'css', 'junior']
+    },
+    {
+        title: "HTML Mastery",
+        pages: 200,
+        author: {
+            name: 'Charlie',
+            age: 50
+        },
+        available: false,
+        price: '48€',
+        tags: ['html', 'advanced', 'junior', 'mid-senior']
+    },
+];
+
+
+// Creare un array (authors) che contiene gli autori dei libri.
+const authors = books.map((book) => {
+    return book.author;
+});
+
+console.log(authors);
+
+
+// Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+const areAuthorsAdult = authors.every((author) => {
+    return author.age >= 18;
+});
+
+console.log(areAuthorsAdult);
+
+
+// Ordina l’array authors in base all’età, senza creare un nuovo array.
+// (se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+
+
+if (areAuthorsAdult) {
+    authors.sort((a, b) => {
+        return a.age - b.age;
+    });
+
+} else {
+    authors.sort((a, b) => {
+        return b.age - a.age;
+    });
+}
+
+
+
+console.log(authors)
+
+
+
